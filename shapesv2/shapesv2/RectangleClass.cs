126326134d12a31c9ae47470abda084d0cf48e81
bobby1072 <75732572+bobby1072@users.noreply.github.com>
2022-06-22 10:56:00 +0100

    Add files via upload

---
+++ shapesv2/shapesv2/RectangleClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double width;
+        private double length;
+        public override string ShapeName { get; }
+        public Rectangle()
+        {
+            ShapeName = "Rectangle";
+        }
+
+        public override void RequestDimensions()
+        {
+            RequestDimensionValue("width", out double Lwidth);
+            RequestDimensionValue("length", out double Llength);
+            width = Lwidth;
+            length = Llength;
+        }
+        public override double CalculateArea()
+        {
+            double Result = length * width;
+            return Result;
+        }
+        public override double CalculatePerimeter()
+        {
+            double Result = (2 * length) + (2 * width); 
+            return Result;
+        }
+    }
+}
